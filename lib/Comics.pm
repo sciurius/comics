5a88d24b73144b591c3ccfb2baf827dd34fa053e
Johan Vromans <jvromans@squirrel.nl>
2016-10-23 17:47:38 +0200

    Add some more files for packaging.

---
+++ lib/Comics.pm
@@ -0,0 +1,27 @@
+#! perl
+
+use strict;
+use warnings
+use utf8;
+use Carp;
+
+package Comics;
+
+our $VERSION = "0.01";
+
+
+
+=head1 LICENSE
+
+Copyright (C) 2016, Johan Vromans,
+
+This module is free software. You can redistribute it and/or
+modify it under the terms of the Artistic License 2.0.
+
+This program is distributed in the hope that it will be useful,
+but without any warranty; without even the implied warranty of
+merchantability or fitness for a particular purpose.
+
+=cut
+
+1;
