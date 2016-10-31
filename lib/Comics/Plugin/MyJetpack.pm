6b698b0119c939552a37db2e636b91bb8987c049
Johan Vromans <jvromans@squirrel.nl>
2016-10-31 16:28:45 +0100

    Refine pattern.

--- lib/Comics/Plugin/MyJetpack.pm
+++ lib/Comics/Plugin/MyJetpack.pm
@@ -7,21 +7,18 @@ package Comics::Plugin::MyJetpack;
 
 use parent qw(Comics::Fetcher::Single);
 
-our $VERSION = "0.01";
+our $VERSION = "0.02";
 
-sub register {
-
-    shift->SUPER::register
-      ( { name    => "You're all just jealous of my Jetpack",
-	  url     => "http://myjetpack.tumblr.com/",
-	  pat     =>
-	    qr{ <img \s+
+our $name    = "You're all just jealous of my Jetpack";
+our $url     = "http://myjetpack.tumblr.com/";
+our $pattern =
+	    qr{ <a \s+
+		 href="http://myjetpack.tumblr.com/image/\d+"> \s*
+		 <img \s+
 		 src="(?<url>http://.*?\.media\.tumblr\.com/
 		 [0-9a-f]+/(?<image>.*?\.\w+))" \s+
-		 alt="For\sthe\sGuardian.?" \s+ /></a>
-	      }xs,
-	} );
-}
+		 alt=".*?" \s+ /></a>
+	      }xs;
 
 # Important: Return the package name!
 __PACKAGE__;
