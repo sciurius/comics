b347542ffe84692c582647c463a4cb39cd8e54b0
Johan Vromans <jvromans@squirrel.nl>
2016-11-01 14:05:37 +0100

    Fix image fetching.

--- lib/Comics/Plugin/LeastICouldDo.pm
+++ lib/Comics/Plugin/LeastICouldDo.pm
@@ -5,27 +5,28 @@ use warnings;
 
 package Comics::Plugin::LeastICouldDo;
 
-use parent qw(Comics::Fetcher::Direct);
+use parent qw(Comics::Fetcher::Cascade);
 
-our $VERSION = "0.02";
+our $VERSION = "0.03";
 
-#### TODO: Need multiple fetcher...
+# Current image is date formatted.
+# Unfortunately, this is not reliable (e.g., on nov 1 the image
+# was 2016/10/20161101.jpg)
 
-sub register {
-
-    # Current image is date formatted.
-    my @tm = localtime(time);
-    my $path = sprintf( "%s/%04d/%02d/%04d%02d%02d.jpg",
-			"wp-content/uploads",
-			1900+$tm[5], 1+$tm[4],
-			1900+$tm[5], 1+$tm[4], $tm[3] );
-
-    shift->SUPER::register
-      ( { name    => "Least I Could Do",
-	  url     => "http://www.leasticoulddo.com/",
-	  path	  => $path,
-	} );
-}
+our $name    = "Least I Could Do";
+our $url     = "http://www.leasticoulddo.com/";
+our @patterns =
+  ( qr{ <a \s+
+	 href="(?<url>http://www.leasticoulddo.com/comic/\d+/)" \s+
+	 id="feature-comic">
+      }six,
+    qr{ <meta \s+
+         property="og:image" \s+
+         content="(?<url>http://www.leasticoulddo.com/wp-content/uploads/
+                   \d+/\d+/(?<image>.*?\.\w+))" \s*
+        />
+      }six,
+  );
 
 # Important: Return the package name!
 __PACKAGE__;
