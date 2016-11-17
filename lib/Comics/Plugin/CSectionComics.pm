f983a53dd7941b2dc8d393edb4258cac7d482a7a
Johan Vromans <jvromans@squirrel.nl>
2016-11-17 21:11:46 +0100

    (CSectionComics) Fix pattern.

--- lib/Comics/Plugin/CSectionComics.pm
+++ lib/Comics/Plugin/CSectionComics.pm
@@ -7,24 +7,20 @@ package Comics::Plugin::CSectionComics;
 
 use parent qw(Comics::Fetcher::Single);
 
-our $VERSION = "0.01";
+our $VERSION = "0.02";
 
-sub register {
-    shift->SUPER::register
-      ( { name    => "C-Section Comics",
-	  url     => "http://www.csectioncomics.com/",
-	  pat	  =>
-	    qr{ <div \s+ id="comic"> \s+
-		<img \s+
-		 src="(?<url>http://cdn.csectioncomics.com/csectioncomics/
-		             wp-content/uploads/\d+/\d+/
-			     (?<image>.*?\.\w+))" \s+
-		 alt="(?<alt>.*?)" \s+
-		 title="(?<title>.*?)" \s*
-		/> \s* </div>
-	      }six,
-	} );
-}
+our $name    = "C-Section Comics";
+our $url     = "http://www.csectioncomics.com/";
+our $pattern =
+  qr{ <div \s+ id="comic"> \s+
+      <img \s+
+       src="(?<url>http://cdn.csectioncomics.com/csectioncomics/
+                   wp-content/uploads/\d+/\d+/
+                   (?<image>.*?\.\w+))" \s+
+       alt="(?<alt>.*?)" \s+
+       title="(?<title>.*?)" \s*
+      />
+    }six;
 
 # Important: Return the package name!
 __PACKAGE__;
