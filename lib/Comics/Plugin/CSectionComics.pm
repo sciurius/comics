bd43a4962aab09bca7516ed29a9b496422e81e49
Johan Vromans <jvromans@squirrel.nl>
2016-11-23 20:55:43 +0100

    Fix pattern.

--- lib/Comics/Plugin/CSectionComics.pm
+++ lib/Comics/Plugin/CSectionComics.pm
@@ -7,14 +7,14 @@ package Comics::Plugin::CSectionComics;
 
 use parent qw(Comics::Fetcher::Single);
 
-our $VERSION = "0.02";
+our $VERSION = "0.03";
 
 our $name    = "C-Section Comics";
 our $url     = "http://www.csectioncomics.com/";
 our $pattern =
   qr{ <div \s+ id="comic"> \s+
       <img \s+
-       src="(?<url>http://cdn.csectioncomics.com/csectioncomics/
+       src="(?<url>http://.*?.csectioncomics.com/csectioncomics/
                    wp-content/uploads/\d+/\d+/
                    (?<image>.*?\.\w+))" \s+
        alt="(?<alt>.*?)" \s+
