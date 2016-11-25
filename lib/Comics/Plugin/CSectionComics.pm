9bd5d77278fdb90b69c6e837f75b00aad0bedc28
Johan Vromans <jvromans@squirrel.nl>
2016-11-25 21:45:25 +0100

    (CSectionComics) Fix pattern.

--- lib/Comics/Plugin/CSectionComics.pm
+++ lib/Comics/Plugin/CSectionComics.pm
@@ -5,22 +5,50 @@ use warnings;
 
 package Comics::Plugin::CSectionComics;
 
-use parent qw(Comics::Fetcher::Single);
+use parent qw(Comics::Fetcher::Cascade);
 
-our $VERSION = "0.03";
+our $VERSION = "0.04";
 
 our $name    = "C-Section Comics";
+
+# C-Section Comics seems to return arbitrary comics.
+
+# our $url     = "http://www.csectioncomics.com/";
+# our $pattern =
+#   qr{ <div \s+ id="comic"> \s+
+#       <img \s+
+#        src="(?<url>http://.*?.csectioncomics.com/csectioncomics/
+#                    wp-content/uploads/\d+/\d+/
+#                    (?<image>.*?\.\w+))" \s+
+#        alt="(?<alt>.*?)" \s+
+#        title="(?<title>.*?)" \s*
+#       />
+#     }six;
+
+# Retrieve from the archives page instead.
+
 our $url     = "http://www.csectioncomics.com/";
-our $pattern =
-  qr{ <div \s+ id="comic"> \s+
-      <img \s+
-       src="(?<url>http://.*?.csectioncomics.com/csectioncomics/
-                   wp-content/uploads/\d+/\d+/
-                   (?<image>.*?\.\w+))" \s+
-       alt="(?<alt>.*?)" \s+
-       title="(?<title>.*?)" \s*
-      />
-    }six;
+our @patterns =
+  (   qr{ <a \s+
+           href="(?<url>http://www.csectioncomics.com/archives)">
+          Comics</a>
+        }six,
+      qr{ <td \s+
+           class="archive-title"> \s*
+          <a \s+
+	   href="(?<url>http://www.csectioncomics.com/comics/.*?)" \s+
+           rel="bookmark"
+        }six,
+      qr{ <div \s+ id="comic"> \s+
+	  <img \s+
+	   src="(?<url>http://.*?.csectioncomics.com/csectioncomics/
+		       wp-content/uploads/\d+/\d+/
+		       (?<image>.*?\.\w+))" \s+
+	   alt="(?<alt>.*?)" \s+
+	   title="(?<title>.*?)" \s*
+	  />
+      }six,
+   );
 
 # Important: Return the package name!
 __PACKAGE__;
