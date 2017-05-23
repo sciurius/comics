cbc3a0b6756b4aeb4604b754245b4710fc412701
Johan Vromans <jvromans@squirrel.nl>
2017-05-23 10:00:22 +0200

    Add LoadingArtist.

---
+++ lib/Comics/Plugin/LoadingArtist.pm
@@ -0,0 +1,26 @@
+#! perl
+
+use strict;
+use warnings;
+
+package Comics::Plugin::LoadingArtist;
+
+use parent qw(Comics::Fetcher::Single);
+
+our $VERSION = "1.00";
+
+our $name    = "Loading Artist";
+our $url     = "http://www.loadingartist.com/latest/";
+
+our $pattern =
+  qr{ <meta \s+ property="og:title" \s+ content="(?<title>.*?)">
+      .*?
+      <div \s+ class="comic"> \s*
+      <img \s+
+       src="(?<url>http://www.loadingartist.com/wp-content/uploads/
+            \d+/\d+/
+            (?<image>.+?\.\w+))"
+    }sx;
+
+# Important: Return the package name!
+__PACKAGE__;
