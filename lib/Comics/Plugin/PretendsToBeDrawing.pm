fb0af3ebec5bed6819c5dd5020bd965471507f08
Johan Vromans <jvromans@squirrel.nl>
2017-06-16 20:55:36 +0200

    (PretendsToBeDrawing) Update pattern.

--- lib/Comics/Plugin/PretendsToBeDrawing.pm
+++ lib/Comics/Plugin/PretendsToBeDrawing.pm
@@ -7,17 +7,18 @@ package Comics::Plugin::PretendsToBeDrawing;
 
 use parent qw(Comics::Fetcher::Single);
 
-our $VERSION = "1.00";
+our $VERSION = "1.01";
 
 our $name    = "Pretends To Be Drawing";
 our $url     = "http://ptbd.jwels.berlin/";
 
 our $pattern =
-  qr{ <div \s+ class="comicwrapper"> \s+
+  qr{ <div \s+ class="comicwrapper" .*? > \s+
       <img \s+
        alt="(?<alt>.*?)" \s+
        title="(?<title>.*?)" \s+
        class="comic" \s+
+       itemprop="image" \s+
        src="(?<url>/comicfiles/full/(?<image>.+?\.\w+))"
     }sx;
 
