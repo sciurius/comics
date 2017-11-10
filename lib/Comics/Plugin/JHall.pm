f08b019379f32bf593c62852fed1758fd615cc5a
Johan Vromans <jvromans@squirrel.nl>
2017-11-10 19:14:18 +0100

    (JHall) Update patterns.

--- lib/Comics/Plugin/JHall.pm
+++ lib/Comics/Plugin/JHall.pm
@@ -7,22 +7,18 @@ package Comics::Plugin::JHall;
 
 use parent qw(Comics::Fetcher::Single);
 
-our $VERSION = "1.00";
+our $VERSION = "1.01";
 
 our $name    = "JHall";
-our $url     = "http://jhallcomics.com/Unsorted/";
+our $url     = "https://jhallcomics.com/";
 
 our $pattern =
-  qr{ <div \s+ class="sixteen\scolumns" \s+ id="comic-container"> \s+
+  qr{ <div \s+ .*? class="image-block-wrapper \s+ .*? > \s*
+      <noscript>
       <img \s+
-       src="(?<url>/sites/default/files/styles/comic/public/(?<image>.+?\.\w+))"
-       .*?
-       <div \s+ class="sixteen\scolumns"> \s+
-         <div \s+ id="comment"> \s+
-	   <p> \s+
-	     <div .*? ><div .*? ><div .*? >
-	       <p>(?<alt> .*? )</p> \s+
-            </div></div></div>
+       src="(?<url>https?://static1.squarespace.com/static/
+                   .*?/t/.*?/.*?/
+		   (?<image>.+?\.\w+))"
     }sx;
 
 # Important: Return the package name!
