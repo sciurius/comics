959d7414dde89ee13ea1bc339fc9203562d20590
Johan Vromans <jvromans@squirrel.nl>
2018-04-18 21:30:20 +0200

    Add DoodleForFood.

---
+++ lib/Comics/Plugin/DoodleForFood.pm
@@ -0,0 +1,26 @@
+#! perl
+
+use strict;
+use warnings;
+
+package Comics::Plugin::DoodleForFood;
+
+use parent qw(Comics::Fetcher::Single);
+
+our $VERSION = "1.00";
+
+our $name    = "Doodle For Food";
+our $url     = "http://www.doodleforfood.com/";
+
+our $pattern =
+  qr{ <figure \s+ class="photo-hires-item".*? > \s*
+      <a \s+ href="https?://www\.doodleforfood\.com/image/\d+"> \s*
+      <img \s+
+       src="?(?<url>https?://\d+\.media\.tumblr\.com/
+                    [0-9a-f]+/
+	            (?<image>tumblr_.+_1280\.\w+))"? \s+
+                    alt="(?<title>.*)
+    }x;
+
+# Important: Return the package name!
+__PACKAGE__;
