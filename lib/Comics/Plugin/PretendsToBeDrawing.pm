4d5ef4dd503521372bb43cdffa770aa3fbf5412c
Johan Vromans <jvromans@squirrel.nl>
2018-06-18 08:42:20 +0200

    Disable PretendsToBeDrawing. We cannot do multi-pane comics yet.

--- lib/Comics/Plugin/PretendsToBeDrawing.pm
+++ lib/Comics/Plugin/PretendsToBeDrawing.pm
@@ -7,9 +7,11 @@ package Comics::Plugin::PretendsToBeDrawing;
 
 #### NOTE: PtbD now uses individual panes for the comic. Hard to do.
 
+our $disabled = 1;
+
 use parent qw(Comics::Fetcher::Single);
 
-our $VERSION = "1.02";
+our $VERSION = "1.03";
 
 our $name    = "Pretends To Be Drawing";
 our $url     = "http://ptbd.jwels.berlin/";
