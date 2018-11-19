a071545d3e76c415d6f1ae5198ce5c81dbe58484
Johan Vromans <jvromans@squirrel.nl>
2018-11-19 14:32:55 +0100

    Fix pattern for Russmo.

--- lib/Comics/Plugin/Russmo.pm
+++ lib/Comics/Plugin/Russmo.pm
@@ -7,16 +7,17 @@ package Comics::Plugin::Russmo;
 
 use parent qw(Comics::Fetcher::Single);
 
-our $VERSION = "1.01";
+our $VERSION = "1.02";
 
 our $name    = "Russmo";
 our $url     = "http://russmo.com";
 our $pattern =
-	    qr{ <img \s+
+	    qr{ <figure \s+ class="wp-block-image \s+ is-resized"> \s*
+		<img \s+
 		src="(?<url>https?://russmo.com/wp-content/
 		uploads/\d+/\d+/(?<image>\d+.+?))" \s+
 	        alt="" \s+
-	        class="wp-image-627"
+	        class="wp-image-\d+"
 	      }x;
 
 # Important: Return the package name!
