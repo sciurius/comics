b13664eaefa3f06ac2722613ce298f63625cdc71
Johan Vromans <jvromans@squirrel.nl>
2019-04-17 19:25:02 +0200

    Add ItchyFeetComic.

---
+++ lib/Comics/Plugin/ItchyFeetComic.pm
@@ -0,0 +1,22 @@
+#! perl
+
+use strict;
+use warnings;
+
+package Comics::Plugin::ItchyFeetComic;
+
+use parent qw(Comics::Fetcher::Single);
+
+our $VERSION = "1.00";
+
+our $name    = "Itchy Feet";
+our $url     = "http://www.itchyfeetcomic.com/";
+our $pattern =
+	    qr{ <img \s+
+		alt="(?<alt>.*?)" \s+
+		src="(?<url>https://i.imgur.com/
+		(?<image>[^./]+\.\w+))"
+	      }x;
+
+# Important: Return the package name!
+__PACKAGE__;
--- lib/Comics/Version.pm
+++ lib/Comics/Version.pm
@@ -1,3 +1,3 @@
 # This file is generated. Do not edit!
 package Comics::Version;
-our $VERSION = "1.110_015";
+our $VERSION = "1.110_016";
