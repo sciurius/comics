e36e5c8d85507d8f2e076f2c1b1827ee32362be3
Johan Vromans <jvromans@squirrel.nl>
2019-08-09 10:12:24 +0200

    Add GrrlPower.

---
+++ lib/Comics/Plugin/GrrlPower.pm
@@ -0,0 +1,26 @@
+#! perl
+
+use strict;
+use warnings;
+
+package Comics::Plugin::GrrlPower;
+
+use parent qw(Comics::Fetcher::Single);
+
+our $VERSION = "1.00";
+
+our $name    = "Grll Power";
+our $url     = "https://grrlpowercomic.com",
+our $pattern =
+  qr{ <div \s+ id="comic"> \s*
+      <img \s+
+       src="(?<url>
+	     https://grrlpowercomic.com/wp-content/uploads/
+	     \d+/\d+/
+	     (?<image>.*?\.\w+))" \s+
+       alt="(?<alt>.*)" \s+
+       title="(?<title>.*?)" \s+
+    }isx;
+
+# Important: Return the package name!
+__PACKAGE__;
