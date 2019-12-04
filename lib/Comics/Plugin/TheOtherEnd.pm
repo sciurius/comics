c23ccc9d3c2b02b1078b4c3c6aae700d00dcaac6
Johan Vromans <jvromans@squirrel.nl>
2019-12-04 13:41:45 +0100

    Fix pattern for TheOtherEnd.

--- lib/Comics/Plugin/TheOtherEnd.pm
+++ lib/Comics/Plugin/TheOtherEnd.pm
@@ -7,14 +7,14 @@ package Comics::Plugin::TheOtherEnd;
 
 use parent qw(Comics::Fetcher::Single);
 
-our $VERSION = "1.00";
+our $VERSION = "1.01";
 
 our $name    = "The Other End";
 our $url     = "http://www.theotherendcomics.com/";
 our $pattern =
 	    qr{ <div \s+ id="comic"> \s+
 		<img \s+
-		 src="(?<url>(?:https?:)?//i2.wp.com/www.kohney.com/
+		 src="(?<url>(?:https?:)?//i..wp.com/www.kohney.com/
 		       wp-content/uploads/\d+/\d+/
 		       (?<image>.*?))"  \s+
 		 alt="(?<alt>.*?)" \s+
