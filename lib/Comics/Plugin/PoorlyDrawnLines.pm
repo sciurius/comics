de40b9c3051bb9aef2de941a6b837db7442118be
Johan Vromans <jvromans@squirrel.nl>
2020-08-05 15:36:30 +0200

    Fix patterns for PoorlyDrawnLines.

--- lib/Comics/Plugin/PoorlyDrawnLines.pm
+++ lib/Comics/Plugin/PoorlyDrawnLines.pm
@@ -7,13 +7,14 @@ package Comics::Plugin::PoorlyDrawnLines;
 
 use parent qw(Comics::Fetcher::Single);
 
-our $VERSION = "1.02";
+our $VERSION = "1.03";
 
 our $name    = "Poorly Drawn Lines";
 our $url     = "http://www.poorlydrawnlines.com/";
 
 our $pattern =
-  qr{ <figure \s+ class="wp-block-image[^"]*"> \s*
+  qr{ <div \s+ class="wp-block-image[^"]*"> \s*
+      <figure \s+ class=".*?">
       <img \s+
        src="(?<url>https?://www.poorlydrawnlines.com/wp-content/uploads/
 	      \d+/\d+/
