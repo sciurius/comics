5f27f1d5bc3284c4956090a8bfa11e94ac06f38c
Johan Vromans <jvromans@squirrel.nl>
2020-08-05 15:47:29 +0200

    Fix patterns for Glasbergen, LittleGamers.

--- lib/Comics/Plugin/Glasbergen.pm
+++ lib/Comics/Plugin/Glasbergen.pm
@@ -7,15 +7,15 @@ package Comics::Plugin::Glasbergen;
 
 use parent qw(Comics::Fetcher::Single);
 
-our $VERSION = "1.04";
+our $VERSION = "1.05";
 
 our $name    = "Glasbergen";
 our $url     = "https://www.glasbergen.com";
 our $pattern =
-	    qr{ <meta \s+
-		 name="twitter:image" \s+
-		 content="(?<url>https?://www.glasbergen.com/wp-content/
-		     uploads/\d+/\d+/(?<image>.+?))"
+	    qr{ <img \s+
+		 class=".*?size-full.*?" \s+
+		 src="(?<url>https?://i2.wp.com/www.glasbergen.com/wp-content/
+		     uploads/\d+/\d+/(?<image>.+?))\?
 	      }x;
 
 # Important: Return the package name!
