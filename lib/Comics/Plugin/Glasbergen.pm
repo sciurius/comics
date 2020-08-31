9a1912219d969efd371d0fe1cdc0e3816d69fd22
Johan Vromans <jvromans@squirrel.nl>
2020-08-31 14:08:47 +0200

    Fix patterns for Glasbergen, DoYouKnowFlo, CSectionComicsBonus.

--- lib/Comics/Plugin/Glasbergen.pm
+++ lib/Comics/Plugin/Glasbergen.pm
@@ -7,15 +7,17 @@ package Comics::Plugin::Glasbergen;
 
 use parent qw(Comics::Fetcher::Single);
 
-our $VERSION = "1.05";
+our $VERSION = "1.06";
 
 our $name    = "Glasbergen";
 our $url     = "https://www.glasbergen.com";
 our $pattern =
 	    qr{ <img \s+
 		 class=".*?size-full.*?" \s+
-		 src="(?<url>https?://i2.wp.com/www.glasbergen.com/wp-content/
-		     uploads/\d+/\d+/(?<image>.+?))\?
+		 src="(?<url>https?:
+		       //(?:glasbergen.b-cdn.net|i2.wp.com/www.glasbergen.com)
+		       /wp-content/
+		     uploads/\d+/\d+/(?<image>.+?))[?"]
 	      }x;
 
 # Important: Return the package name!
