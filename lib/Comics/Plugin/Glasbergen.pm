6289f1deccc3d13efe107d1ca314b18fcde24306
Johan Vromans <jvromans@squirrel.nl>
2020-11-06 10:08:40 +0100

    (Glasbergen) Fix pattern.

--- lib/Comics/Plugin/Glasbergen.pm
+++ lib/Comics/Plugin/Glasbergen.pm
@@ -7,7 +7,7 @@ package Comics::Plugin::Glasbergen;
 
 use parent qw(Comics::Fetcher::Single);
 
-our $VERSION = "1.06";
+our $VERSION = "1.07";
 
 our $name    = "Glasbergen";
 our $url     = "https://www.glasbergen.com";
@@ -15,7 +15,7 @@ our $pattern =
 	    qr{ <img \s+
 		 class=".*?size-full.*?" \s+
 		 src="(?<url>https?:
-		       //(?:glasbergen.b-cdn.net|i2.wp.com/www.glasbergen.com)
+		       //(?:www.glasbergen.com|glasbergen.b-cdn.net|i2.wp.com/www.glasbergen.com)
 		       /wp-content/
 		     uploads/\d+/\d+/(?<image>.+?))[?"]
 	      }x;
