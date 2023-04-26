69aeab217b1a727e9a1d7aa4a24e80e4d6cd71d6
Johan Vromans <jvromans@squirrel.nl>
2023-04-26 19:46:04 +0200

    Fix patterns for CommitStrip.

--- lib/Comics/Plugin/CommitStrip.pm
+++ lib/Comics/Plugin/CommitStrip.pm
@@ -7,7 +7,7 @@ package Comics::Plugin::CommitStrip;
 
 use parent qw(Comics::Fetcher::Cascade);
 
-our $VERSION = "1.01";
+our $VERSION = "1.02";
 
 our $name     = "CommitStrip";
 our $url      = "https://www.commitstrip.com/";
@@ -17,13 +17,13 @@ our @patterns =
 		     \d\d\d\d / \d\d / \d\d / .*? /)">
 	      }x,
 	    qr{ <img \s+
+		class="aligncenter \s+ size-full.*?" \s+
 		src="(?<url>https://www.commitstrip.com/wp-content/uploads/
 		       \d+/\d+/
 		       (?<image>.*?\.\w+))" \s+
 		alt="(?<alt>.*?)" \s+
 		width="\d+" \s+
-		height="\d+" \s+
-		class="alignnone \s+ size-full.*?" \s+ />
+		height="\d+" \s+ />
 	      }x
 	  );
 
