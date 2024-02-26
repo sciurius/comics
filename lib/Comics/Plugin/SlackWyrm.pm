7c32175b9dc8f8e4faa494737892a3e3225b232b
Johan Vromans <jvromans@squirrel.nl>
2024-02-26 10:12:29 +0100

    Fix patterns for SlackWyrm.

--- lib/Comics/Plugin/SlackWyrm.pm
+++ lib/Comics/Plugin/SlackWyrm.pm
@@ -7,14 +7,14 @@ package Comics::Plugin::SlackWyrm;
 
 use parent qw(Comics::Fetcher::Single);
 
-our $VERSION = "1.03";
+our $VERSION = "1.04";
 
 our $name    = "Slack Wyrm";
 our $url     = "https://www.joshuawright.net/";
 
 our $pattern =
   qr{ 
-       src="(?<url>images/(?<image>\d+%20-%20slack%20wyrm%20-%20[\w%]+\.\w+))\?crc=\d+"
+       src="(?<url>images/(?<image>picture%20-%20slackwyrm%20[\w%]+\.\w+))\?crc=\d+"
     }x;
 
 # Important: Return the package name!
