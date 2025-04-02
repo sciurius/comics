c46690e4e58eaaa25d8f9e20d0d399d41e73e875
Johan Vromans <jvromans@squirrel.nl>
2025-04-02 08:04:57 +0200

    Fix GoComics fetcher.

--- lib/Comics/Fetcher/GoComics.pm
+++ lib/Comics/Fetcher/GoComics.pm
@@ -16,12 +16,9 @@ Comics::Fetcher::GoComics -- Fetcher for GoComics.
   package Comics::Plugin::Garfield;
   use parent qw(Comics::Fetcher::GoComics);
 
-  sub register {
-      shift->SUPER::register
-	( { name    => "Garfield",
-	    url     => "http://www.comics.com/garfield",
-	  } );
-  }
+  our $name    = "Garfield";
+  our $url     = "https://www.comics.com/garfield";
+
   # Return the package name.
   __PACKAGE__;
 
@@ -54,10 +51,11 @@ Fetcher specific arguments:
 
 =cut
 
-our $VERSION = "1.03";
+our $VERSION = "1.04";
 
 # Page contents changed, january 10, 2017.
 # Page contents changed, april 5, 2018.
+# Page contents changed, april 1, 2025.
 
 sub register {
     my ( $pkg, $init ) = @_;
@@ -72,12 +70,8 @@ sub register {
     # Add the standard pattern for GoComics comics.
     $self->{patterns} =
       [
-       qr{ href="(?<url>.*?)">Comics</a>
-         }x,
-       qr{ <meta \s+ property="og:image" \s+
-	   content="(?<url>https?://featureassets.amuniversal.com/assets/
-	   (?<image>[0-9a-f]+))" \s+
-           />
+       qr{ (?<url>https://featureassets.gocomics.com/assets/
+	   (?<image>[0-9a-f]+)) \?
          }x,
        ];
 
