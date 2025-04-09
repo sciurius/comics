7829fbff4fe3443d426dc0a642ed439eed760e9b
Johan Vromans <jvromans@squirrel.nl>
2025-04-09 11:05:43 +0200

    Fix FokkeEnSukke.

--- lib/Comics/Plugin/FokkeEnSukke.pm
+++ lib/Comics/Plugin/FokkeEnSukke.pm
@@ -7,7 +7,7 @@ package Comics::Plugin::FokkeEnSukke;
 
 use parent qw(Comics::Fetcher::Single);
 
-our $VERSION = "1.00";
+our $VERSION = "1.01";
 
 our $name = "Fokke en Sukke";
 our $url = "https://www.nrc.nl/fokke-sukke/";
@@ -15,9 +15,12 @@ our $url = "https://www.nrc.nl/fokke-sukke/";
 # See TODO in Base.pm.
 
 our $pattern =
-  qr{ <meta \s+ property="og:image" \s+
-	content=".*/s3(?<url>/static.nrc.nl/images/gn4/stripped/(?<image>.*\.jpg))"
-  }x;
+  qr{ <picture \s+ class="dmt-featured-image__picture"> \s*
+      <source \s+
+      srcset=".*?
+      (?<url>https://images.nrc.nl/.*?/
+	  static.nrc.nl/images/gn4/stripped/(?<image>data[-0-9a-f]+\.\w+))
+  }xs;
 
 # Important: Return the package name!
 __PACKAGE__;
