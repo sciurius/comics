74b4532dc90c99fdbd59e35221748e2421214b42
Johan Vromans <jvromans@squirrel.nl>
2026-01-13 17:25:05 +0100

    Fix LICD.

--- lib/Comics/Plugin/LeastICouldDo.pm
+++ lib/Comics/Plugin/LeastICouldDo.pm
@@ -7,25 +7,21 @@ package Comics::Plugin::LeastICouldDo;
 
 use parent qw(Comics::Fetcher::Cascade);
 
-our $VERSION = "1.04";
+our $VERSION = "1.05";
 
 our $name    = "Least I Could Do";
 our $url     = "https://www.leasticoulddo.com/";
+
+my @tm    = localtime(time);
+my $year  = sprintf("%04d", 1900+$tm[5]);
+my $yr    = substr($year, 2);
+my $month = sprintf("%02d", 1+$tm[4]);
+my $day   = sprintf("%02d", $tm[3]);
+
 our @patterns =
-  ( qr{ <a \s+
-	 href="(?<url>https?://(?:www.)?leasticoulddo.com/comic/\d+/?)" \s+
-	 id="latest-comic">
-      }six,
-    qr{ <meta \s+
-         property="og:description" \s+
-	 content="(?<title>.*?)" \s*
-	/>
-	.*?
-	<meta \s+
-         property="og:image" \s+
-         content="(?<url>https?://(?:www.)?leasticoulddo.com/wp-content/uploads/
-                   \d+/\d+/(?<image>.*?\.\w+))" \s*
-        />
+  ( qr{ (?<url>https?://(?:www.)?leasticoulddo.com/
+	    wp-content/uploads/$year/$month/
+	    (?<image>(?:beg|licd)\d+-[a-z]+${day}_${yr}_desktop-2048x.*?\.jpg))
       }six,
   );
 
